ca1677d75abb56c2b35016828457be4177d52fbd
Web-projektitII <jukka.aula@kolumbus.fi>
2024-01-18 11:45:25 +0200

    Responsiivisuutta
    Co-authored-by: dprogrammergurl <dprogrammergurl@users.noreply.github.com>
    Co-authored-by: Timo Anjala <FakeManiGH@users.noreply.github.com>
    Co-authored-by: Eevame <Eevame@users.noreply.github.com>
    Co-authored-by: Marek Männiksaar <m.manniksaar@gmail.com>
    Co-authored-by: otavo002-xamk <otavo002-xamk@users.noreply.github.com>

--- header.php
+++ header.php
@@ -1,4 +1,38 @@
 <?php
-echo "<div class='navigointipalkki'>Tähän tulee navigointipalkki.</div>";
+$active = basename($_SERVER['PHP_SELF'],'.php');
+function active($sivu,$active){
+    return $active == $sivu ? 'active' : '';  
+    }
 ?>
-<p>Tähän tulee jotakin muuta headeriä.</p>
+<!DOCTYPE html>
+<html lang="en"> <!--En on oikein-->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta description="joku kuvausteksti" author="joku tekijä">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<link rel="stylesheet" href="navbar.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
+<link rel="stylesheet" href="site.css">
+<?php
+if (isset($css)) echo "<link rel='stylesheet' href='$css'>";
+if (isset($js)) echo "<script defer src='$js'></script>";
+?>
+<title><?=$title?></title>
+<script>
+</script>
+</head>
+<body>
+<nav>
+<a class="brand-logo" href="index.php">
+<img src="omniamusta_tausta.png" alt="Logo"></a>
+<input type="checkbox" id="toggle-btn">
+<label for="toggle-btn" class="icon open"><i class="fa fa-bars"></i></label>
+<label for="toggle-btn" class="icon close"><i class="fa fa-times"></i></label>
+<?php
+echo "<a class='".active('index',$active). "' href='index.php'>Kotisivu</a>";
+echo "<a class='".active('sivumalli',$active). "' href='sivumalli.php'>Sivumalli</a>";
+/* Huom. tästä oikeaan laitaan. */
+echo "<a class='nav-suojaus ".active('phpinfo',$active). "' href='phpinfo.php'>phpinfo</a>";
+?>
+</nav>
